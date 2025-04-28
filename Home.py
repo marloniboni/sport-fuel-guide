79c1861674102fe39a28b85161d1e0f04e8a32d6
Lucasw02-creator <lucas.weilenmann@student.unisg.ch>
2025-04-28 10:04:40 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -7,7 +7,7 @@ st.title("🏡 Sport Fuel Guide")
 st.markdown("Willkommen! Diese App hilft dir bei der Planung deiner Trainings- und Wettkampfernährung.")
 
 # Insert Picture before the inputs
-st.image("https://images.ctfassets.net/aytpbz9e0sd0/5n9hvfTT9hG7QxgveI7E3W/58dfe5751c5aef4155e60f110fa6f1cd/Peter-Stetina-with-CLIF-SHOT-Energy-Gel-riding-bike.jpg?w=1920.png", use_container_width=True)
+st.image("https://images.ctfassets.net/aytpbz9e0sd0/5n9hvfTT9hG7QxgveI7E3W/58dfe5751c5aef4155e60f110fa6f1cd/Peter-Stetina-with-CLIF-SHOT-Energy-Gel-riding-bike.jpg?w=1920", use_container_width=800)
 # Benutzerangaben
 gewicht = st.slider("Gewicht (in kg)", min_value=40, max_value=150, value=70, step=1)
 groesse = st.slider("Körpergröße (in cm)", min_value=140, max_value=210, value=175, step=1)
