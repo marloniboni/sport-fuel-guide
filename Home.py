aec638ed57eb7771bbd23fa4c5b2ecdb1cab110b
marloniboni <marlongamiett@gmail.com>
2025-05-12 23:58:09 +0200

    Update Home.py

--- Home.py
+++ Home.py
@@ -1,26 +1,30 @@
-import streamlit as st
-import requests
-import urllib
+import streamlit as st       #Importiert Streamlit zur Erstellung einer Web-App (Grundvoraussetzung)
+import requests              #Wird für HTTP-Anfragen verwendet
+import urllib                #Wird zur URL-Verarbeitung verwendet
 
+
+# Seiteneinstellungen also Titel, Icon, Layoutbreite -> für ästhetische Ansicht
 st.set_page_config(
     page_title="Sport Fuel Guide",
     page_icon=None,
     layout="wide"
 )
-st.title("Sport Fuel Guide")
-st.info("Diese App hilft dir bei der Planung deiner Trainings- und Wettkampfernährung.")
-with st.expander("Gib deine Daten ein"):
-    col1, col2 = st.columns(2)
-    with col1:
-        gewicht    = st.slider("Gewicht (kg)", 40, 150, 70)
+
+st.title("Sport Fuel Guide")                                                                            #Titel der Home-Seite
+st.info("Diese App hilft dir bei der Planung deiner Trainings- und Wettkampfernährung.")                #Infobox unterhalb des Titels
+
+with st.expander("Gib deine Daten ein"):                                        #Aufklappbarer Eingabereich
+    col1, col2 = st.columns(2)                                                  #Bildung von 2 Spalten: wobei 1. Spalte mit Gewicht und Alter per Slider bestimmt werden kann und Spalte 2 
+    with col1:                                                                  #Wobei 1. Spalte mit Gewicht und Alter per Slider bestimmt werden kann
+        gewicht    = st.slider("Gewicht (kg)", 40, 150, 70)                     #Spalte 2 Grösse per Slider und Geschlecht mit Auswahlbox gewählt werden kann
         alter      = st.slider("Alter (Jahre)", 12, 80, 25)
     with col2:
         groesse    = st.slider("Körpergröße (cm)", 140, 210, 175)
         geschlecht = st.selectbox("Geschlecht", ["Männlich", "Weiblich"])
         
-# Grundumsatz & Flüssigkeit
+# Grundumsatz & Flüssigkeitsbedarf Berechnungen
 if geschlecht == "Männlich":
-    grundumsatz = 66.47 + (13.7 * gewicht) + (5.0 * groesse) - (6.8 * alter)
+    grundumsatz = 66.47 + (13.7 * gewicht) + (5.0 * groesse) - (6.8 * alter)    #Grundumsatz Kcal abhängig von Geschlecht, Gewicht, Alter und Grösse
 else:
     grundumsatz = 655.1 + (9.6 * gewicht) + (1.8 * groesse) - (4.7 * alter)
 
